4a85ec9954234afe58d9bf724d134f40678229e3
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-08-13 14:04:33 -0300

    step 8 done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import TodoApp from '@/components/TodoApp'
 import './index.css'
+import '@/styles/app.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>,
 )
